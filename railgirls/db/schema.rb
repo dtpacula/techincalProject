285dc0f1168ef8dcc48c14b93b154c191c02719a
dtpacula <Derek@wifi-196-35.unwired.unomaha.edu>
2014-09-29 14:43:59 -0500

    Updated Files for To Do App

--- railgirls/db/schema.rb
+++ railgirls/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140929180035) do
+ActiveRecord::Schema.define(version: 20140929194130) do
 
   create_table "ideas", force: true do |t|
     t.string   "name"
